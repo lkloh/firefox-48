a9d195ad2afc0c5e1ad81b878321fd9b9b6ac90d
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2016-10-28 17:57:08 -0400

    gecko

--- blog_2016.py
+++ blog_2016.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-rom selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from xvfbwrapper import Xvfb
 
 # def init_driver():
