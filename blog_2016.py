402bdec57dec4a729f4909730c42053b4947bd0a
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2016-10-28 17:48:26 -0400

    gecko

--- blog_2016.py
+++ blog_2016.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from xvfbwrapper import Xvfb
 
 # def init_driver():
 # 	driver = webdriver.Firefox()
@@ -13,6 +14,8 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 def init_driver():
 	# binary = FirefoxBinary('/usr0/lloh/firefox-48/geckodriver')
+	vdisplay = Xvfb(width=1280, height=720)
+	vdisplay.start()
 	driver = webdriver.Firefox()
 	driver.wait = WebDriverWait(driver, 2)
 	return driver
